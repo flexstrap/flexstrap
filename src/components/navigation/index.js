5525907d5f3779654015006c248d6bea1ee2a7f0
Chris Abrams <mail@chrisabrams.com>
2015-01-05 20:21:04 -0500

    Flexbox Grid basics; Nav Component for Mobile
    Also started Header Component.

---
+++ src/components/navigation/index.js
@@ -0,0 +1,39 @@
+var Component;
+
+try { Component = require('../base/component') } catch(e) { Component = Flexstrap.Component }
+
+class NavigationComponent extends Component {
+
+  constructor(options = {}) {
+    super(options)
+
+    var $navigation = this.$el,
+        $menuIcon   = $navigation.find('.icon-menu'),
+        $menuToggle = $navigation.find('.navigation-toggle')
+
+    $navigation.on('click', (e) => {
+      e.preventDefault()
+
+      if($menuIcon.hasClass('animate-rotate-half')) {
+
+        $menuIcon.removeClass('animate-rotate-half')
+        $menuIcon.addClass('animate-counter-rotate-half')
+
+      }
+
+      else {
+
+        $menuIcon.removeClass('animate-counter-rotate-half')
+        $menuIcon.addClass('animate-rotate-half')
+
+      }
+
+      $navigation.toggleClass('navigation-open')
+
+    })
+
+  }
+
+}
+
+try { module.exports = NavigationComponent } catch(e) { Flexstrap.NavigationComponent = NavigationComponent }
